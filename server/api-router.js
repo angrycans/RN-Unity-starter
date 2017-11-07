f789428d2f10672ea119f7885b64913b2faf332e
angrycans <angrycans@gmail.com>
2017-11-07 12:52:56 +0800

    zip ok

--- server/api-router.js
+++ server/api-router.js
@@ -1,14 +1,30 @@
 var Router = require('koa-router');
+var fs = require('fs');
 var ExFile = require('./utils/exfile.js');
+
 
 var ApiRouter = new Router({
   prefix: '/api'
 });
 
 
-ApiRouter.get('/allfiles', async ctx => {
-    ctx.body = ExFile.getAllFiles(__dirname + '/../unity/Assets/GameApp/LuaScripts');
+ApiRouter.get('/files/list', async ctx => {
+  ctx.body = ExFile.list(__dirname + '/../unity/Assets/GameApp/LuaScripts');
 });
 
+ApiRouter.get('/files/zip', async ctx => {
+  //console.log(ctx);
+  let zip = await ExFile.zip(__dirname + '/../unity/Assets/GameApp/LuaScripts');
+  if (!zip.err) {
 
-exports.ApiRouter=ApiRouter;
+    ctx.set('Content-disposition', 'attachment; filename=' + zip.filename);
+    // ctx.set('Content-type', 'zip');
+    ctx.type = 'zip';
+    ctx.body = fs.createReadStream(zip.filepath);
+  } else {
+    ctx.body = zip.filename + 'create error:' + zip.err;
+  }
+
+});
+
+exports.ApiRouter = ApiRouter;
--- server/utils/exfile.js
+++ server/utils/exfile.js
@@ -1,22 +1,79 @@
 'use strict';
 var fs = require('fs');
+var os = require('os');
+var path = require('path');
 
+var archiver = require('archiver');
 
-function getAllFiles(dirpath){
-    //console.log("get files")
-    var res = [] , files = fs.readdirSync(dirpath);
-    files.forEach(function(file){
-      var pathname = dirpath+'/'+file
+function list(dirpath) {
+  //console.log("get files")
+  var res = [], files = fs.readdirSync(dirpath);
+  files.forEach(function (file) {
+    var pathname = dirpath + '/' + file
       , stat = fs.lstatSync(pathname);
-  
-     if (!stat.isDirectory()){
-        res.push(pathname);
-     } else {
-        res = res.concat(getAllFiles(pathname));
-     }
-   });
-   return res
+
+    if (!stat.isDirectory()) {
+      res.push(pathname);
+    } else {
+      res = res.concat(getAllFiles(pathname));
+    }
+  });
+  return res
+}
+
+async function zip(dirpath) {
+  return new Promise((resolve, reject) => {
+    var tmpzipfilename = os.tmpdir + '/' + path.basename(dirpath) + '.zip'
+    console.log("tmpzipfilename", tmpzipfilename);
+    var output = fs.createWriteStream(tmpzipfilename);
+    var zipArchive = archiver('zip');
+
+    output.on('close', async function () {
+      console.log('archiver has been finalized and the output file descriptor has closed.');
+      console.log(zipArchive.pointer() + ' total bytes');
+
+      resolve({ err: null, filepath: tmpzipfilename, filename: path.basename(dirpath) + '.zip' });
+    });
+    output.on('end', function () {
+      console.log('Data has been drained');
+    });
+
+    // good practice to catch warnings (ie stat failures and other non-blocking errors)
+    zipArchive.on('warning', function (err) {
+      console.log(zipArchive.on('warning'), err);
+    });
+
+    // good practice to catch this error explicitly
+    zipArchive.on('error', function (err) {
+      console.log(zipArchive.on('error'), err);
+      resolve({ err: err, filepath: tmpzipfilename, filename: path.basename(dirpath) + '.zip' });
+
+    });
+
+
+
+    zipArchive.pipe(output);
+
+    //zipArchive.directory(dirpath, false);
+    zipArchive.glob("**/*.txt",
+      {
+        cwd: dirpath,
+        // ignore: ['*.meta']
+      },
+      {
+        prefix: path.basename(dirpath)
+      }
+    );
+
+    zipArchive.finalize(function () {
+      console.log('finalize');
+
+    });
+
+  });
 }
 
 
-exports.getAllFiles=getAllFiles;
+
+exports.list = list;
+exports.zip = zip;
